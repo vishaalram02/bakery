22b1aa9d5f67329d387ebc1835de2d2229697dba
vishaalram02 <vishaalram02@gmail.com>
2023-07-04 14:46:03 -0400

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const nocache = require('nocache')
 const cors = require('cors')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 5000
 
 var secret = "bakersgonnabake"
 
